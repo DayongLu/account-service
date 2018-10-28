d14360e7df30fccfc26f3ee6bf095b1d27d53dcf
DayongLu <ludayong20@gmail.com>
2018-10-28 17:51:27 -0400

    add age

--- src/main/java/mega/coffee/account/repo/AccountRepo.java
+++ src/main/java/mega/coffee/account/repo/AccountRepo.java
@@ -3,6 +3,7 @@ package mega.coffee.account.repo;
 import mega.coffee.account.model.Account;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+
 public interface AccountRepo extends MongoRepository<Account, String> {
     Account findAccountByLoginName(String loginName);
 }
